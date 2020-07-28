34a46746026f9f565b04ce3c2f8c918d373d3d6b
Francisco Viana <franciscoviana862@gmail.com>
2020-07-28 16:00:45 -0300

    configuracao do swagger

--- back-end/src/serve.js
+++ back-end/src/serve.js
@@ -1,12 +1,15 @@
 import express from "express";
 import routes from "./routes";
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConfig from './config/swagger.json';
 
 const app = express();
 
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 app.listen(3000);
-console.log('okk')
