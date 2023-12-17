6c79c6dce3827537f7fb48f6aed549ce1488be3f
Francisco Viana <franciscoviana862@gmail.com>
2023-12-17 14:11:12 -0300

    melhorias na estrutura

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
       host: "127.0.0.1",
       user: "postgres",
       password: "postgres",
-      database: "development",
+      database: "syseps_db",
       charset: "utf8",
     },
     migrations: {
